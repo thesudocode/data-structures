fe95dd91a73265f00d40772ef1fc97cae7cbb26e
Nikhil Arora <narora200@gmail.com>
2017-07-08 22:36:31 +0530

    Linked List: Basic implementation (Part 2)

--- LinkedList.java
+++ LinkedList.java
@@ -4,4 +4,15 @@ package com.iamoperand.datastructures;
  * Created by iamoperand on 8/7/17.
  */
 public class LinkedList {
+
+    Node head;
+
+    class Node{
+        int data;
+        Node next;
+
+        Node(int d){data = d};
+    }
+
+
 }
